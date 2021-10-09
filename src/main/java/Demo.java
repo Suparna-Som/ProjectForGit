2e65e9f1f31dbd1ce51fe1f224c9bd4b04529673
Suparna-Som <92136435+Suparna-Som@users.noreply.github.com>
2021-10-09 14:27:47 +0530

    Update Demo.java

--- src/main/java/Demo.java
+++ src/main/java/Demo.java
@@ -1,9 +1,15 @@
 public class Demo {
+    public static add(int a,int b)
+    {
+        int c = a+b;
+        System.out.println("Additon"+c);
+    }
     public static void main (String args[])
     {
         System.out.println("New to Git hub");
         System.out.println("Git is Amazing");
         System.out.println("I am  learning git");
         System.out.println("Wow...");
+        
     }
 }
